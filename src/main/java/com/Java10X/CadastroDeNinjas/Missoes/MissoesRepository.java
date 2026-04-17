7be1f3e0a13f729648d2a1edca2c315b291c1b6d
victorllunes <victorllunes@gmail.com>
2026-04-16 22:13:51 -0300

    feat: create Interface MissoesRepository

---
+++ src/main/java/com/Java10X/CadastroDeNinjas/Missoes/MissoesRepository.java
@@ -0,0 +1,5 @@
+package com.Java10X.CadastroDeNinjas.Missoes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissoesRepository extends JpaRepository<MissoesModel, Long> { }
