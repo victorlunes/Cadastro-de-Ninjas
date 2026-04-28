4dda6c2237ef9081e51d724d0b0c94e02ad1f613
victorllunes <victorllunes@gmail.com>
2026-04-27 21:11:05 -0300

    feat: create endpoints in controller

--- src/main/java/com/Java10X/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/com/Java10X/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,6 +1,9 @@
 package com.Java10X.CadastroDeNinjas.Ninjas;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,4 +14,26 @@ public class NinjaController {
         return "Essa é minha primeira mensagem nessa rota";
     }
 
+    @GetMapping("/buscar-ninja")
+    public String buscarNinja() {
+        return "Ninja encontrado com sucesso";
+    }
+
+    @GetMapping("/buscar-ninja/{id}")
+    public String buscarNinjaPorId(String id) {
+        return "Ninja encontrado com sucesso: " + id;
+    }
+
+    @PostMapping("/adicionar-ninja")
+    public String criarNinja() {
+        return "Ninja criado com sucesso";
+    }
+
+    @PutMapping("alterar-ninja/{id}")
+    public void alterarNinja(String id) {
+        
+    }
+
+    @DeleteMapping("/deletar-ninja/{id}")
+    public void deletarNinja(String id) {}
 }
