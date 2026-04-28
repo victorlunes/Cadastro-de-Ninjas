645e9ffa6b1dddd00cadc8b428c8ff7df4390a8b
victorllunes <victorllunes@gmail.com>
2026-04-27 21:40:50 -0300

    fix: ajust name controller Ninja

--- src/main/java/com/Java10X/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/com/Java10X/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -4,15 +4,12 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/ninjas")
 public class NinjaController {
-
-    @GetMapping("/boas-vindas")
-    public String boasVindas(){
-        return "Essa é minha primeira mensagem nessa rota";
-    }
 
     @GetMapping("/buscar-ninja")
     public String buscarNinja() {
